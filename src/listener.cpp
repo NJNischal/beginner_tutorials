5583d2d353e1c56c46e3de239605777c8b78a87b
nischal <njnischal@gmail.com>
2019-11-04 01:06:32 -0500

    [listener.cpp] Added license and doxygen comments to listener file

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,8 +1,22 @@
+/**
+*
+*Copyright (c) 2019 Nagireddi Jagadesh Nischal
+*
+*Redistribution and use in source and binary forms, with or without modification, are permitted *provided that the following conditions are met:
+*
+*1. Redistributions of source code must retain the above copyright notice, this list of conditions and *the following disclaimer.
+*
+*2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions *and the following disclaimer in the documentation and/or other materials provided with the *distribution.
+*
+*3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse *or promote products derived from this software without specific prior written permission.
+*
+*THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR *IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND *FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR *CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL *DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, *DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER *IN *CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT *OF THE *USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
 /*
  * @copyright 2019
- * @copyright BSD License
+ * @copyright BSD 3-Clause
  * @file listener.cpp
- * Design
  * @author Nagireddi Jagadesh Nischal
  * @date 10/28/2019
  * @brief Implementation of Subscriber node which reads custom string message
@@ -18,6 +32,12 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
+/**
+ * @brief      the main function of the talker file
+ * @param      argc  Specifies the number of elements within argv
+ * @param      argv  Array of c-string pointers
+ * @return     0 int
+ */
 int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
